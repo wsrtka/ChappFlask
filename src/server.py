512f58ab7ba1f8cf6f23596d6fb4c999cc5e391d
wsrtka <witold.serwatka@pm.me>
2020-07-20 14:30:38 +0200

    added simple login method

--- src/server.py
+++ src/server.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, request, abort
 
 
 app = Flask(__name__)
 
 
+users = []
+
+
 @app.route('/')
-def hello(request):
+def hello():
     return "Hello world"
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json['username']
+
+    if username in users:
+        abort(403)
+
+    else:
+        users.append(username)
+        return 'Login successfull'
 
 
 if __name__ == '__main__':
